fbd3f4c765ed1597ed28e6a086573116d8847f79
Sishi Dev <sishifinancedev@gmail.com>
2021-03-18 09:31:17 +0700

    tmp

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,14 +1,10 @@
 export default {
   cake: {
-    56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
-    97: '',
-  },
-  tokenz: {
-    56: '0xfadb0dac0cee280ea760eb08316545258afd9e8c',
+    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
     97: '',
   },
   masterChef: {
-    56: '0x034e28a7abbc1aa96c62a0b05ae51a4f971c4d99',
+    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
     97: '',
   },
   wbnb: {
--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -1,25 +1,6 @@
 import { Ifo } from './types'
 
 const ifos: Ifo[] = [
-  // {
-  //   id: 'tokenz',
-  //   address: '0xfadb0dac0cee280ea760eb08316545258afd9e8c',
-  //   isActive: false,
-  //   name: 'TokenZ (TOKENZ)',
-  //   subTitle: 'TOKENZ Farming BSC',
-  //   description:
-  //     'BLINk enables BNB holders to enjoy gaming experiences that WINk users on Tron have been enjoying for over two years.',
-  //   launchDate: 'Nov. 20',
-  //   launchTime: '9PM SGT',
-  //   saleAmount: '100,000,000 BLINK',
-  //   raiseAmount: '$1,000,000',
-  //   cakeToBurn: '$500,000',
-  //   projectSiteUrl: 'https://blink.wink.org',
-  //   currency: 'CAKE-BNB LP',
-  //   currencyAddress: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
-  //   tokenDecimals: 10,
-  //   releaseBlockNumber: 3279767,
-  // },
   {
     id: 'blink',
     address: '0x44a9Cc8463EC00937242b660BF65B10365d99baD',
