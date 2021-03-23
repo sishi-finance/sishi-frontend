567c7df75149672edaff9373b37876396249aabe
Sishi Dev <sishifinancedev@gmail.com>
2021-03-23 10:51:03 +0700

    update text

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -121,7 +121,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
     : '-'
 
   const lpLabel = farm.lpSymbol
-  const earnLabel = 'EGG'
+  const earnLabel = 'SISHI'
   const farmAPY =
     farm.apy &&
     farm.apy.times(new BigNumber(100)).toNumber().toLocaleString(undefined, {
@@ -133,7 +133,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
 
   return (
     <FCard>
-      {farm.tokenSymbol === 'EGG' && <StyledCardAccent />}
+      {farm.tokenSymbol === 'SISHI' && <StyledCardAccent />}
       <CardHeading
         lpLabel={lpLabel}
         multiplier={farm.multiplier}
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -60,9 +60,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'Goose Finance')}
+          {TranslateString(576, 'Sishi Finance')}
         </Heading>
-        <Text>{TranslateString(578, 'Top 3 best DEFI app on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'The Next-Gen Yield Farming on BSC- Maximum Total Supply 21 Millions SISHI')}</Text>
       </Hero>
       <div>
         <Cards>
